b3fc2bd298efe5db95b642444c2e6bd4182738f3
Forrest Almasi <forrestalmasi@gmail.com>
2015-11-23 00:40:28 -0500

    two pages

--- routes.js
+++ routes.js
@@ -2,23 +2,118 @@ var express = require('express');
 var fs = require('fs-extended');
 var router = express.Router();
 
+var files = fs.readdirSync('./public/images/marble/');
 
+var links = [
+    {
+        url: 'mailto:_@forrestalmasi.com',
+        text: '_@forrestalmasi.com'
+    },
+    {
+        url: 'http://twitter.com/forrestalmasi',
+        text: 'twitter.com/forrestalmasi'
+    },    {
+        url: 'http://twitter.com/everyphotoshop',
+        text: 'twitter.com/everyphotoshop'
+    },
+    {
+        url: 'http://raretides.tumblr.com',
+        text: 'raretides.tumblr.com'
+    },    {
+        url: 'http://raretines.tumblr.com',
+        text: 'raretines.tumblr.com'
+    },
+    {
+        url: 'https://github.com/malls',
+        text: 'github.com/malls'
+    },
+    {
+        url: 'http://isitflagday.us/',
+        text: 'isitflagday.us'
+    },
+    {
+        url: 'http://gardenparty.club',
+        text: 'gardenparty.club'
+    }
+];
+
+var friends = [
+    {
+        url: 'http://dallegretto.com/',
+        text: 'dan allegretto'
+    },
+    {
+        url: 'http://www.albedell.com/',
+        text: 'al bedell'
+    },
+    {
+        url: 'http://sammckinniss.com/',
+        text: 'sam mckinniss'
+    },
+    {
+        url: 'http://brycegrat.es/',
+        text: 'bryce grates'
+    },
+    {
+        url: 'http://www.sigridlauren.com/',
+        text: 'sigrid lauren'
+    },
+    {
+        url: 'http://kerrydoran.net/',
+        text: 'kerry doran'
+    },
+    {
+        url: 'http://mikepepi.com/',
+        text: 'mike pepi'
+    },
+    {
+        url: 'http://tcour.com/',
+        text: 'brian droitcour'
+    },
+    {
+        url: 'http://jasonrivette.com/',
+        text: 'jason rivette'
+    },
+    {
+        url: 'http://darciewilder.com/',
+        text: 'darcie wilder'
+    }
+];
 
 function getMarble() {
-    var files = fs.readdirSync('./public/images/marble/');
     var index = Math.floor((Math.random() * (files.length - 1) + 1));
-    console.log(index)
     return files[index];
 }
-
-console.log(getMarble());
 
 /* GET home page. */
 router.get('/', function(req, res) {
     var marble = getMarble();
     res.render('parking', {marble: marble});
-  // res.render('index', { title: 'Forrest Almasi <3 <3' });
 });
+
+router.get('/friends', function(req, res) {
+    var marble = getMarble();
+    res.render('links', {marble: marble, links: friends, heading: 'these are my friends check out their websites'});
+});
+
+router.get('/links', function(req, res) {
+    var marble = getMarble();
+    res.render('links', {marble: marble, links: links, heading: 'please enjoy these links to stuff by me'});
+});
+
+    
+// http://www.maskmagazine.com/the-substance-issue/life/interview-the-thread    
+// http://dismagazine.com/disco/51435/the-real-remixes-hit-the-beach/
+
+// router.get('/guestbook', function(req, res) {
+//     var marble = getMarble();
+//     res.render('guestbook', {marble: marble});
+// });
+
+// router.post('/guesbook', function(req, res) {
+//     var marble = getMarble();
+//     res.render('guestbook', {marble: marble});
+// });
 
 router.get('/persona', function(req, res) {
     var images = new Array();
@@ -33,8 +128,9 @@ router.get('/persona', function(req, res) {
 
 });
 
-router.get('/:view', function(req, res) {
-  res.render(req.params.view);
-});
+// angular thing
+// router.get('/:view', function(req, res) {
+//   res.render(req.params.view);
+// });
 
 module.exports = router;
